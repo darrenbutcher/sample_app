5770d5e1bb0dd8315311bb36655c006310545b26
Darren Butcher <dbutcher6@gmail.com>
2011-05-21 05:38:27 -0400

    Added layout links (with routes)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  
+
   #Return a title on a per-page basis.
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
@@ -9,4 +9,9 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
+  root :to => "pages#home"
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -60,3 +61,4 @@ SampleApp::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 end
+
